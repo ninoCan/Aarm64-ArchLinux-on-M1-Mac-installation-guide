1acd96801babbf51e84cd8addf4d3b9539099c0e
nino Cangialosi <nino.cangialosi@wolt.com>
2022-05-22 07:24:43 +0300

    refactor: fix installation to be minimal

--- alpine-draft.sh
+++ alpine-draft.sh
@@ -1,38 +1,36 @@
+#### Minimal alpine setup
+#### ref: https://wiki.alpinelinux.org/wiki/Alpine_setup_scripts#setup-alpine
+
 # Start systemclock
 service hwclock start
 
-# Add google DNS to resolve addresses
-cat <<EOF >> /etc/resolv.conf
-namespace 8.8.8.8
-namespace 8.8.4.4
+# Set up interfaces
+cat <<EOF > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+    hostname alpine-test
 EOF
 
 # Prevent overwriting of /etc/resolv.conf
 echo 'RESOLV_CONF="no"' >> /etc/udhcpd.conf
 
-## TODO improve these two commented blocks
-## ref: https://wiki.alpinelinux.org/wiki/Alpine_setup_scripts#setup-alpine
-# Prepare answerfile for setup-alpine
-#cat <<EOF > answerfile
-## Use US layout with US variant
-#KEYMAPOPTS="us us"
-#
-## Set hostname to localhost
-#HOSTNAMEOPTS="-n localhost"
-#
-#APKCHACEOPTS="/var/cache/apk"
-#EOF
-# NOT READY YET
+# Start networking daemon
+/etc/init.d/networking --quiet start &
 
-# Call setup-alpine from file
-# setup-alpine -f answerfile
-# NOT READY YET, run it by hand and answer the questions:
-setup-alpine -a
+# Add google DNS to resolve addresses
+setup-dns -d google.com -n 8.8.8.8 8.8.4.4
+
+# Add a random repo to the mirros
+setup-apkrepos -r
 
 # Setup tools needed to install arch
-apk add parted e2fsprogs libarchive-tools efibootmgr curl
+apk add curl e2fsprogs efibootmgr libarchive-tools parted
 
-# src: https://ktprograms.codeberg.page/blog/posts/2022-03-17_1750_utm-arch-arm/
+#### Partition the virtual device
+
 # Create a GPT partition table
 parted /dev/vda -- mklabel gpt
 # Create a 256M EFI System Partition
@@ -46,11 +44,12 @@ parted /dev/vda -- mkpart '"Arch Linux root"' ext4 512MB 100%
 mkfs.vfat -F 32 -n boot /dev/vda1
 mkfs.ext4 -L archlinux /dev/vda2
 
-
 # Mount the ext4 root partition onto /mnt/ by running
 mount -t ext4 /dev/vda2 /mnt/
 # Create the /mnt/boot/ folder to mount the EFI System Partition and mount it
 mkdir -p /mnt/boot/ && mount -t vfat /dev/vda1 /mnt/boot/
+
+#### Download and install Archlinux
 
 # Download the latest Archlinux ARM image and mount it
 curl -L http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz | bsdtar -xpC /mnt/
@@ -58,7 +57,8 @@ curl -L http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz | bsdta
 # Modify the fstab file to define ho to mount partitions
 echo '/dev/vda1 /boot vfat defaults 0 2' >> /mnt/etc/fstab
 
-# Initialize the EFI Boot Stub: https://www.kernel.org/doc/html/latest/admin-guide/efi-stub.html
+# Initialize the EFI Boot Stub
+# ref: https://www.kernel.org/doc/html/latest/admin-guide/efi-stub.html
 efibootmgr --create \ # create new boot entry
 --disk /dev/vda \ # point at the virtual disk
 --part 1 \ # specify the boot partition
